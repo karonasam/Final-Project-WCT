bd6ae47fa3dbd4bacd92d2fdb4052b1b7259a3f8
admin <karonasam40@gmail.com>
2026-07-31 15:30:41 +0700

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ command }) => ({
-  plugins: [tailwindcss(), react()],
-  base: command === "build" ? "/bookverse/" : "/",
-}));
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  base: "/bookverse/",
+});
