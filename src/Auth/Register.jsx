e498a1660800ba36e47639a2273b45234742622d
admin <karonasam40@gmail.com>
2026-07-31 19:29:33 +0700

    update

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {register} from "./authService";
 import "./Auth.css";
+import auth from "../firebase/firebase";
 
 export default function Register() {
     const navigate = useNavigate();
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-  base: "/bookverse/",
-});
+export default defineConfig(({ command }) => ({
+  plugins: [tailwindcss(), react()],
+  base: command === "build" ? "/bookverse/" : "/",
+}));
