3056290536dcf26a6468d860dc72c27c15ccc19e
admin <karonasam40@gmail.com>
2026-07-31 15:02:54 +0700

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        react()
-    ],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  base: "/bookverse/",
+});
