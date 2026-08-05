50541996bc8627a4789cd546ce93be9e8642a6e5
admin <karonasam40@gmail.com>
2026-08-05 15:22:23 +0700

    Use HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './Auth/AuthService.jsx'
 import { CartProvider } from './context/CartContext.jsx'
@@ -9,12 +9,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
