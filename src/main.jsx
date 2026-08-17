620e7f558b0d80460062c65062c36030dadfd50e
admin <karonasam40@gmail.com>
2026-08-17 21:58:29 +0700

    Fix Tailwind and Vite configuration

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './Auth/AuthService.jsx'
 import { CartProvider } from './context/CartContext.jsx'
-import "tailwindcss";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/react-part3/",
+  base: "/Final-Project-WCT/",
 });
